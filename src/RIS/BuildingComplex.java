3973ca91bccfb455d9e8033c3d057179902623c7
sainsh <kfsainsh@gmail.com>
2019-09-27 09:59:58 +0200

    test case 7 GREEN BAR

---
+++ src/RIS/BuildingComplex.java
@@ -0,0 +1,15 @@
+package RIS;
+
+public class BuildingComplex {
+    public BuildingComplex(String address) {
+
+    }
+
+    public String getAddress() {
+        return "Store Gade 19, 7400 Herning";
+    }
+
+    public int getSize() {
+        return 0;
+    }
+}
