f05be3899835a59ef820b6bf0d5d8dc0233f38e9
sainsh <kfsainsh@gmail.com>
2019-09-27 10:03:13 +0200

    implemented test case 8

--- test/RIS/BuildingComplexTest.java
+++ test/RIS/BuildingComplexTest.java
@@ -2,6 +2,8 @@ package RIS;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 public class BuildingComplexTest extends TestCase {
 
     public void testCreateEmptyBuildingComplex(){
@@ -11,5 +13,18 @@ public class BuildingComplexTest extends TestCase {
         assertEquals("Store Gade 19, 7400 Herning", buildingComplex.getAddress());
         assertEquals(0, buildingComplex.getSize());
 
+    }
+
+    public void testCreateBuildingComplex(){
+
+        BuildingComplex complex = new BuildingComplex("Lille Gade 14, 7430 Ikast",  new ArrayList<Apartment>(){
+            {
+                add(new Apartment(40,0));
+                add(new Apartment(40,0));
+            }
+        });
+
+        assertEquals(80, complex.getSize());
+        assertEquals("Lille Gade 14, 7430 Ikast", complex.getAddress());
     }
 }
