e8debb21e0ce442a5d0c246d5c4aa288bbd4f114
sainsh <kfsainsh@gmail.com>
2019-09-27 09:15:39 +0200

    implemented test case 2

--- test/RIS/RoomTest.java
+++ test/RIS/RoomTest.java
@@ -4,12 +4,20 @@ import junit.framework.TestCase;
 
 public class RoomTest extends TestCase {
 
-    public void testCreateRoom(){
+    public void testCreateRoomWithFakeIt(){
 
         Room room = new Room(Room.type.Kitchen, 14);
 
         assertEquals(14, room.getSqr());
         assertEquals(Room.type.Kitchen, room.getType());
+    }
+
+    public void testCreateRoom(){
+
+        Room room = new Room(Room.type.Bathroom,9);
+
+        assertEquals(9, room.getSqr());
+        assertEquals(Room.type.Bathroom, room.getType());
     }
 
 
