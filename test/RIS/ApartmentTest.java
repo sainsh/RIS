a69863c6704550169d17adcd10c55949e67df167
sainsh <kfsainsh@gmail.com>
2019-09-27 09:46:49 +0200

    implemented test case 5

--- test/RIS/ApartmentTest.java
+++ test/RIS/ApartmentTest.java
@@ -2,13 +2,30 @@ package RIS;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 public class ApartmentTest extends TestCase {
 
     public void testCreateEmptyApartment(){
 
-        Apartment apartment = new Apartment();
+        Apartment apartment = new Apartment(null);
 
         assertEquals(0,apartment.getSize());
         assertEquals(0, apartment.getRoomCount());
+    }
+
+    public void testCreateApartment(){
+
+        Apartment apartment = new Apartment(new ArrayList<Room>(){
+                {
+                 add(new Room(Room.Type.Kitchen,12));
+                 add(new Room(Room.Type.Bathroom, 7));
+                 add(new Room(Room.Type.Room,25));
+                }
+        });
+
+        assertEquals(59, apartment.getSize());
+        assertEquals(4, apartment.getRoomCount());
+
     }
 }
